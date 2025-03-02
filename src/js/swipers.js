b454a998981dd99c78b13b78481b6eb745518dca
nXs <nxs88@mail.ru>
2025-03-03 03:37:49 +0700

    update

--- src/js/swipers.js
+++ src/js/swipers.js
@@ -4,114 +4,73 @@ import { Pagination } from 'swiper/modules'
 import 'swiper/swiper.min.css'
 import 'swiper/modules/pagination.min.css'
 
-const swiperElement = document.querySelector('.swiper');
-const repairSwiperElement = document.querySelector('.repair-swiper');
-const priceSwiperElement = document.querySelector('.price-swiper');
+const swiperElement = document.querySelector('.swiper')
+const repairSwiperElement = document.querySelector('.repair-swiper')
+const priceSwiperElement = document.querySelector('.price-swiper')
 
-let swiper = null;
-let repairSwiper = null;
-let priceSwiper = null;
+let swiper = null
+let repairSwiper = null
+let priceSwiper = null
 
 function checkSwiper() {
   if (window.innerWidth >= 768) {
-    // Скрываем все свайперы на ширине больше 768px и уничтожаем их
     if (swiperElement) swiperElement.style.display = 'none';
     if (repairSwiperElement) repairSwiperElement.style.display = 'none';
     if (priceSwiperElement) priceSwiperElement.style.display = 'none';
 
-    // Уничтожаем существующие свайперы
-    if (swiper) {
-      swiper.destroy(true, true);
-      swiper = null;
-    }
-    if (repairSwiper) {
-      repairSwiper.destroy(true, true);
-      repairSwiper = null;
-    }
-    if (priceSwiper) {
-      priceSwiper.destroy(true, true);
-      priceSwiper = null;
-    }
+    if (swiper) swiper.destroy(true, true);
+    if (repairSwiper) repairSwiper.destroy(true, true);
+    if (priceSwiper) priceSwiper.destroy(true, true);
+
+    swiper = null;
+    repairSwiper = null;
+    priceSwiper = null;
   } else {
-    // Показываем свайперы на меньшей ширине и инициализируем их
     if (swiperElement) swiperElement.style.display = 'block';
     if (repairSwiperElement) repairSwiperElement.style.display = 'block';
     if (priceSwiperElement) priceSwiperElement.style.display = 'block';
 
-    // Инициализация первого свайпера
     if (!swiper && swiperElement) {
       swiper = new Swiper('.swiper', {
         modules: [Pagination],
-        breakpoints: {
-          490: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          640: {
-            slidesPerView: 3,
-            spaceBetween: 90,
-          },
-        },
+        slidesPerView: 'auto',
+        spaceBetween: 16,
         loop: true,
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination-main', 
           clickable: true,
         },
       });
-    } else if (swiper) {
-      swiper.update();
     }
-
-    // Инициализация второго свайпера
+    
     if (!repairSwiper && repairSwiperElement) {
       repairSwiper = new Swiper('.repair-swiper', {
         modules: [Pagination],
-        breakpoints: {
-          490: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          640: {
-            slidesPerView: 3,
-            spaceBetween: 90,
-          },
-        },
+        slidesPerView: 'auto',
         loop: true,
+        spaceBetween: 16,
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination-repair', 
           clickable: true,
         },
       });
-    } else if (repairSwiper) {
-      repairSwiper.update();
     }
-
-    // Инициализация третьего свайпера
+    
     if (!priceSwiper && priceSwiperElement) {
       priceSwiper = new Swiper('.price-swiper', {
         modules: [Pagination],
-        breakpoints: {
-          490: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          640: {
-            slidesPerView: 3,
-            spaceBetween: 90,
-          },
-        },
+        slidesPerView: 'auto',
+        spaceBetween: 16,
         loop: true,
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination-price', 
           clickable: true,
         },
       });
-    } else if (priceSwiper) {
-      priceSwiper.update();
     }
   }
 }
 
 // Добавляем обработчик события resize и запускаем функцию при загрузке страницы
-window.addEventListener('resize', checkSwiper);
-checkSwiper();
+window.addEventListener('resize', checkSwiper)
+checkSwiper()
